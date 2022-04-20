21fa6600993d26c0b933a430c27e04a894d45dfc
hall488 <hall488@purdue.edu>
2022-04-20 14:16:19 -0400

    dd

--- edgetpuvision/detect.py
+++ edgetpuvision/detect.py
@@ -259,8 +259,17 @@ def add_render_gen_args(parser):
     parser.add_argument('--print', default=False, action='store_true',
                         help='Print inference results')
 
+@app.route("/")
 def main():
     run_app(add_render_gen_args, render_gen)
+    templateData = {
+    }
+   # Pass the template data into the template main.html and return it to the user
+    return render_template('main.html', **templateData)
+
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+    print("yo")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
