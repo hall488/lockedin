4d49350050dbe51182779df7b849cc0ed074d96c
hall488 <hall488@purdue.edu>
2022-04-20 15:04:25 -0400

    dd

--- edgetpuvision/detect.py
+++ edgetpuvision/detect.py
@@ -272,7 +272,7 @@ def main():
 #    print("yo")
 
 if __name__ == '__main__':    
-    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     main()
     
     
