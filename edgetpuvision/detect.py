f4ed2016d45d9f65959b61b9ec02f6aae5918e25
hall488 <hall488@purdue.edu>
2022-04-21 03:58:19 -0400

    D

--- edgetpuvision/detect.py
+++ edgetpuvision/detect.py
@@ -122,7 +122,7 @@ def overlay(title, objs, get_color, labels, inference_time, inference_rate, layo
         if labels:
             caption = '%d%% %d %d %s' % (percent, bbox.xmin, bbox.ymin, labels[obj.id])
         else:
-            caption = '%d %d %d' % (x + w/2, y + h/2, int(encoder()))
+            caption = '%d %d %d' % (x + w/2, y + h/2, int(encoder() or 0))
 
 
         motor_IO(x +w/2, y+h/2)
