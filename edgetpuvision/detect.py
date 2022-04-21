d229f432525cc70743102027a447b717c2e4c21f
hall488 <hall488@purdue.edu>
2022-04-21 03:51:41 -0400

    D

--- edgetpuvision/detect.py
+++ edgetpuvision/detect.py
@@ -122,7 +122,7 @@ def overlay(title, objs, get_color, labels, inference_time, inference_rate, layo
         if labels:
             caption = '%d%% %d %d %s' % (percent, bbox.xmin, bbox.ymin, labels[obj.id])
         else:
-            caption = '%d %d' % (x + w/2, y + h/2)
+            caption = '%d %d %d' % (x + w/2, y + h/2, encoder())
 
 
         motor_IO(x +w/2, y+h/2)
@@ -200,7 +200,7 @@ def encoder():
 
 
 def motor_IO(x, y):
-    sys.stdout.write(str(encoder()))
+    #sys.stdout.write(str(encoder()))
     if x > 500 :
         in1.write(True)
         in2.write(False)
